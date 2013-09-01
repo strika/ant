eba2c469209e2d87b0de7f96e5ca64fe3e50012d
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-09-01 11:56:37 +0200

    Implement basic World

--- src/World.js
+++ src/World.js
@@ -1,2 +1,29 @@
 function World() {
+  this.liveCells = [];
 }
+
+World.prototype.getLiveCells = function() {
+  return this.liveCells;
+};
+
+World.prototype.isCellAlive = function(x, y) {
+  return _.any(this.liveCells, function(cell) {
+    return cell[0] === x && cell[1] === y;
+  });
+};
+
+World.prototype.isCellDead = function(x, y) {
+  return !this.isCellAlive(x, y);
+};
+
+World.prototype.reviveCell = function(x, y) {
+  if (this.isCellDead(x, y)) {
+    this.liveCells.push([x, y]);
+  }
+};
+
+World.prototype.killCell = function(x, y) {
+  this.liveCells = _.reject(this.liveCells, function(cell) {
+    return cell[0] === x && cell[1] === y;
+  });
+};
