e51c84a1faab132110ee0b6c1cfa5af66bc89573
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-09-02 18:48:26 +0200

    Ant can turn around

--- spec/AntSpec.js
+++ spec/AntSpec.js
@@ -5,9 +5,101 @@ describe("Ant", function() {
     ant = new Ant();
   });
 
-  describe("#say", function() {
-    it("says 'hello'", function() {
-      expect(ant.say()).toEqual("hello");
+  describe("#getDirection", function() {
+    it("returns 'up'", function() {
+      expect(ant.getDirection()).toEqual("up");
+    });
+  });
+
+  describe("#turnLeft", function() {
+    describe("when direction is up", function() {
+      it("turns ant left", function() {
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("left");
+      });
+    });
+
+    describe("when direction is left", function() {
+      beforeEach(function() {
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("left");
+      });
+
+      it("turns ant down", function() {
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("down");
+      });
+    });
+
+    describe("when direction is down", function() {
+      beforeEach(function() {
+        ant.turnLeft();
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("down");
+      });
+
+      it("turns ant right", function() {
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("right");
+      });
+    });
+
+    describe("when direction is right", function() {
+      beforeEach(function() {
+        ant.turnRight();
+        expect(ant.getDirection()).toEqual("right");
+      });
+
+      it("turns ant up", function() {
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("up");
+      });
+    });
+  });
+
+  describe("#turnRight", function() {
+    describe("when direction is up", function() {
+      it("turns ant right", function() {
+        ant.turnRight();
+        expect(ant.getDirection()).toEqual("right");
+      });
+    });
+
+    describe("when direction is left", function() {
+      beforeEach(function() {
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("left");
+      });
+
+      it("turns ant up", function() {
+        ant.turnRight();
+        expect(ant.getDirection()).toEqual("up");
+      });
+    });
+
+    describe("when direction is down", function() {
+      beforeEach(function() {
+        ant.turnLeft();
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("down");
+      });
+
+      it("turns ant left", function() {
+        ant.turnRight();
+        expect(ant.getDirection()).toEqual("left");
+      });
+    });
+
+    describe("when direction is right", function() {
+      beforeEach(function() {
+        ant.turnRight();
+        expect(ant.getDirection()).toEqual("right");
+      });
+
+      it("turns ant down", function() {
+        ant.turnRight();
+        expect(ant.getDirection()).toEqual("down");
+      });
     });
   });
 });
--- src/Ant.js
+++ src/Ant.js
@@ -1,6 +1,26 @@
 function Ant() {
+  this.direction = "up";
+  this.directions = ["up", "right", "down", "left", "up", "right", "down", "left"];
 }
 
-Ant.prototype.say = function() {
-  return "hello";
+Ant.prototype.getDirection = function() {
+  return this.direction;
+};
+
+Ant.prototype.turnLeft = function() {
+  var currentDirectionIndex = _.lastIndexOf(this.directions, this.direction);
+  var nextDirectionIndex = currentDirectionIndex - 1;
+
+  this.direction = this.directions[nextDirectionIndex];
+};
+
+Ant.prototype.turnRight = function() {
+  var currentDirectionIndex = _.indexOf(this.directions, this.direction);
+  var nextDirectionIndex = currentDirectionIndex + 1;
+
+  this.direction = this.directions[nextDirectionIndex];
+};
+
+Ant.prototype.getCurrentDirectionIndex = function() {
+  return _.indexOf(this.directions, this.direction);
 };
