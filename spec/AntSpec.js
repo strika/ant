9303414baa6e694e36e92ff316fabe2d3d146f7d
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-09-02 19:12:26 +0200

    Ant can move around

--- spec/AntSpec.js
+++ spec/AntSpec.js
@@ -1,13 +1,21 @@
 describe("Ant", function() {
+  var x = 1;
+  var y = 2;
   var ant;
 
   beforeEach(function() {
-    ant = new Ant();
+    ant = new Ant(x, y);
   });
 
   describe("#getDirection", function() {
     it("returns 'up'", function() {
       expect(ant.getDirection()).toEqual("up");
+    });
+  });
+  
+  describe("#getLocation", function() {
+    it("returns ants location", function() {
+      expect(ant.getLocation()).toEqual([x, y]);
     });
   });
 
@@ -99,6 +107,52 @@ describe("Ant", function() {
       it("turns ant down", function() {
         ant.turnRight();
         expect(ant.getDirection()).toEqual("down");
+      });
+    });
+  });
+
+  describe("#moveForward", function() {
+    describe("when direction is up", function() {
+      it("moves ant up", function() {
+        ant.moveForward();
+        expect(ant.getLocation()).toEqual([x - 1, y]);
+      });
+    });
+
+    describe("when direction is left", function() {
+      beforeEach(function() {
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("left");
+      });
+
+      it("moves ant left", function() {
+        ant.moveForward();
+        expect(ant.getLocation()).toEqual([x, y - 1]);
+      });
+    });
+
+    describe("when direction is down", function() {
+      beforeEach(function() {
+        ant.turnLeft();
+        ant.turnLeft();
+        expect(ant.getDirection()).toEqual("down");
+      });
+
+      it("moves ant down", function() {
+        ant.moveForward();
+        expect(ant.getLocation()).toEqual([x + 1, y]);
+      });
+    });
+
+    describe("when direction is right", function() {
+      beforeEach(function() {
+        ant.turnRight();
+        expect(ant.getDirection()).toEqual("right");
+      });
+
+      it("moves ant right", function() {
+        ant.moveForward();
+        expect(ant.getLocation()).toEqual([x, y + 1]);
       });
     });
   });
--- src/Ant.js
+++ src/Ant.js
@@ -1,10 +1,16 @@
-function Ant() {
+function Ant(x, y) {
+  this.x = x;
+  this.y = y;
   this.direction = "up";
   this.directions = ["up", "right", "down", "left", "up", "right", "down", "left"];
 }
 
 Ant.prototype.getDirection = function() {
   return this.direction;
+};
+
+Ant.prototype.getLocation = function() {
+  return [this.x, this.y];
 };
 
 Ant.prototype.turnLeft = function() {
@@ -21,6 +27,14 @@ Ant.prototype.turnRight = function() {
   this.direction = this.directions[nextDirectionIndex];
 };
 
-Ant.prototype.getCurrentDirectionIndex = function() {
-  return _.indexOf(this.directions, this.direction);
+Ant.prototype.moveForward = function() {
+  if (this.direction == "up") {
+    this.x = this.x - 1;
+  } else if (this.direction == "right") {
+    this.y = this.y + 1;
+  } else if (this.direction == "down") {
+    this.x = this.x + 1;
+  } else if (this.direction == "left") {
+    this.y = this.y - 1;
+  }
 };
